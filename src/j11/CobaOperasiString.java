3bd7dc4d6efe422504fd181e171ce04b59b0f3d2
Ilham <Ilham@LAPTOP-0BO61VTL>
2018-08-13 13:41:06 +0700

    Identitas

---
+++ src/j11/CobaOperasiString.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j11;
+
+/**
+ *
+ * @author Ilham
+ */
+public class CobaOperasiString {
+    public static void main(String[] args) {
+        String identitas = "Ilham Khalis Putra Agatha/ X RPL 2/ 17";
+        System.out.println("Identitas : " + identitas);
+    }
+    
+}
