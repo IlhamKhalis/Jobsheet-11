48bf9382e5b6a84f64ba92bb3dbab76a91ec0f95
Ilham <Ilham@LAPTOP-0BO61VTL>
2018-08-14 09:22:18 +0700

    Tambah Input

--- src/j11/ConvertVokal2Angka.java
+++ src/j11/ConvertVokal2Angka.java
@@ -18,5 +18,14 @@ public class ConvertVokal2Angka {
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka\n");
     }
+    private static String tampilInput() {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Masukan Kalimat : ");
+    String kalimat = scanner.nextLine();
+    System.out.println("Kalimat Asli : " + kalimat );
+    
+    return kalimat;
+    }
     
 }
