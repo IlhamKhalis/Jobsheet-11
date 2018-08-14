7fe6193d40099cfd9fd44cc391060b34c2efeca1
Ilham <Ilham@LAPTOP-0BO61VTL>
2018-08-14 09:21:00 +0700

    Initial Commit

---
+++ src/j11/ConvertVokal2Angka.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ilham
+ */
+public class ConvertVokal2Angka {
+    private static void tampilJudul(String identitas) 
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka\n");
+    }
+    
+}
