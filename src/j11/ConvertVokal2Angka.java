9eaa830fc3f46321e1f7ee2b212fbe79d1da8b6d
Ilham <Ilham@LAPTOP-0BO61VTL>
2018-08-14 09:24:46 +0700

    Tambah Tampil Hasil

--- src/j11/ConvertVokal2Angka.java
+++ src/j11/ConvertVokal2Angka.java
@@ -44,5 +44,8 @@ public class ConvertVokal2Angka {
         for(int i=0;i<arrKal.length;i++)
             System.out.println(arrKal[i]+" => "+arrCon[i]);
     }
+    private static void tampilHasil(String convert) {
+        System.out.println("Kalimat Alay Angka : " + convert);
+    }
     
 }
